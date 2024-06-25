40b0815ce2aae768f4ffdb2b447edf05556d66b9
Dimas Halim Hartanto <dimashalimgit@gmail.com>
2024-06-25 20:45:55 +0700

    update pembuatan view obat

--- app/Http/Controllers/Home/ObatController.php
+++ app/Http/Controllers/Home/ObatController.php
@@ -7,5 +7,11 @@ use Illuminate\Http\Request;
 
 class ObatController extends Controller
 {
-    //
+    public function obat()
+    {
+        $pageConfigs = ['pageHeader' => false];
+
+        // return view page untuk detail obat
+        return view('', ['pageConfigs' => $pageConfigs]);
+    }
 }
